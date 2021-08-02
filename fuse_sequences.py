fa1b24468341f7c4eddcf19ac97fe4e07ba02775
Panagiotis (Panos) Adam <88230846+precambriangoat@users.noreply.github.com>
2021-08-02 10:36:57 +0200

    Update fuse_sequences.py

--- fuse_sequences.py
+++ fuse_sequences.py
@@ -1,11 +1,11 @@
-##Copyright 2021 GEORGE E KOLYFETIS
+##The MIT License (MIT) Copyright (c) 2021 GEORGE E KOLYFETIS 
 ##Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 ##The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 ##THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 ## This script fuses (interactively) sequences from an alignment. It is used for sequences that are split (e.g. due to sequencing or ORF prediction errors), using their accession numbers as a guide.
 
-#usage : python fuse_sequences.py alignment_file.aln logfile.txt outfile.txt 
+#usage : python fuse_sequences.py {fasta_file.faa} {logfile.txt} {outfile.faa} 
 
 import sys
 import os

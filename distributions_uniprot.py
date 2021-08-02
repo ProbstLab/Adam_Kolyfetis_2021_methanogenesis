d933c3bf53a24a21388a94837a259d6cbf663619
Panagiotis (Panos) Adam <88230846+precambriangoat@users.noreply.github.com>
2021-08-02 10:35:48 +0200

    Update distributions_uniprot.py

--- distributions_uniprot.py
+++ distributions_uniprot.py
@@ -1,12 +1,12 @@
-##Copyright 2021 GEORGE E KOLYFETIS
+##The MIT License (MIT) Copyright (c) 2021 GEORGE E KOLYFETIS 
 ##Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 ##The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 ##THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 ## This script starts by searching each accession from a list of Pfam accessions (given by the user as a text file) in a Uniprot records file (given again by the user). For each Pfam accession it outputs the number for each Domain
-## of life (plus Viruses and Unclassified organisms) this accession can be found in plus the percentage of all those records that corresponds to Archaea only.
+## of life (plus Viruses and Unclassified organisms) this accession can be found in, plus the percentage of Archaea in each of those records.
 
-#usage : python distributions_uniprot.py pfam_accessions.txt uniprot_records.txt
+#usage : python distributions_uniprot.py {pfam_accessions.txt} {uniprot_records.txt}
 
 
 import Bio.SwissProt as sp
